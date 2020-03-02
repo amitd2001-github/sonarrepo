da4fde0962598df14dcb99a61bcc753079b72a26
AMITD <amitd_2001@yahoo.com>
2020-03-02 10:26:19 +0530

    first sonar commit

---
+++ HelloWorldSonar.java
@@ -0,0 +1,14 @@
+
+public class HelloWorldSonar {
+
+	public static int i;
+	
+	public static void main(String[] args) {
+		//for(int i=0;i<15;i++) {
+			System.out.println("i" + i);
+		//}
+		
+
+	}
+
+}
